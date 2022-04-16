0bbe1501599044f70b7890933767e42e38742a27
N2WU <49031851+N2WU@users.noreply.github.com>
2022-04-16 10:42:58 -0400

    Update speaker_test.ino

--- speaker_test.ino
+++ speaker_test.ino
@@ -1,10 +1,13 @@
 #include <toneAC.h>
+#include "LowPower.h"
 
 const int xpin = A3;                  // x-axis of the accelerometer
 const int ypin = A2;                  // y-axis
 const int zpin = A1;                  // z-axis (only on 3-axis models)
 
 //speaker must be pin 9 and 10
+
+int freq = 1000;
 
 void setup() {
 
@@ -14,10 +17,14 @@ void loop() {
   
   if (analogRead(xpin) == 0 && analogRead(ypin) == 0 && analogRead(zpin) == 0){
     toneAC(freq); // Play frequency
-    delay(100);
+    delay(1000);
+    toneAC(); //no tone
+    delay(1000);
   }
   
+  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, 
+                SPI_OFF, USART0_OFF, TWI_OFF);
   toneAC(); // Play no frequency
-  delay(100);     // Wait 1 ms for loop
+  delay(100);     // Wait 100 ms for loop
   
 }
